ed582f2408cd894074c2c49ec0fc7e82bec7aa57
shanmukha123307 <anusha.palgot@gmail.com>
2025-12-14 20:34:49 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,6 @@
+a = input("Enter a word: ")
+for i in a:
+    if (i == 'A'):
+        print ("A is found")
+    else:
+        print ("A not found")
---
+++ written.py
@@ -0,0 +1,14 @@
+def find_first_set_bit(n):
+    if n == 0:
+        return -1  
+
+    position = 0
+    while (n & 1) == 0:
+        n >>= 1
+        position += 1
+
+    return position
+
+number = 8  
+first_set_bit_position = find_first_set_bit(number)
+print(f"First set bit position: {first_set_bit_position}")
